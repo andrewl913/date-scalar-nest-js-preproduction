0f666435b5d0a7184d69189e69f861e7fff36d49
Andrew Lincoln <andrewl913@yahoo.com>
2020-06-08 10:22:26 -0400

    Add a comment in the module for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLFederationModule, GraphQLModule } from "@nestjs/graphql";
 import { AppResolver } from './app.resolver';
 
 @Module({
+  // Change to GraphQLModule here for a working example.
   imports: [GraphQLFederationModule.forRoot({
     autoSchemaFile: "src/schema.graphql",
     path: "/test"
